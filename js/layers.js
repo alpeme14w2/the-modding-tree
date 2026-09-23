2ec6c9e596091a4a257817b951a116f76ca06869
alpeme14w2 <110390625+alpeme14w2@users.noreply.github.com>
2026-09-23 15:08:29 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -102,7 +102,7 @@ addLayer("rng", {
         11: {
             canClick() {return true},
             onClick() {
-                return massroll2(65536,Math.max(toNumber(player.rng.points),1e9))
+                return Math.max(massroll2(65536,Math.max(toNumber(player.rng.points),1e9)),toNumber(player.rng.points))
             },
             display() {return toNumber(player.rng.points)},
         }
